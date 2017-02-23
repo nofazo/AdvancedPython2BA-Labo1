51514274af8f26ce983f41889f0152798cc6fc04
nofazo <nouhi.f.z@gmail.com>
2017-02-23 17:38:46 +0100

    Fix utils module to pass tests

--- utils.py
+++ utils.py
@@ -9,6 +9,21 @@ def fact(n):
     Post: Returns the factorial of 'n'.
     Throws: ValueError if n < 0
     """
+    try:
+
+      if n== 0 or n==1:
+        return 1
+
+      elif n>1:
+        return n * fact(n - 1)
+
+      else:
+         return print("Veuillez entrer un nombre entier positif")
+
+    except:
+        return print("Veuillez entrer un nombre correct")
+
+
 
 def roots(a, b, c):
     """Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -17,7 +32,18 @@ def roots(a, b, c):
     Post: Returns a tuple with zero, one or two elements corresponding
           to the roots of the ax^2 + bx + c polynomial.
     """
-    pass
+    delta = (b ** 2) - 4 * a * c
+
+    if delta == 0:
+        return (0, 0)
+
+    if delta < 0:
+        return ("None", "None")
+
+    if delta > 0:
+        x = (-b + sqrt(delta)) / (2 * a)
+        y = (-b - sqrt(delta)) / (2 * a)
+    return (x, y)
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
@@ -28,7 +54,14 @@ def integrate(function, lower, upper):
     Post: Returns an approximation of the integral from 'lower' to 'upper'
           of the specified 'function'.
     """
-    pass
+    a = lower
+    sum = 0
+    while a <= upper:
+        sum += eval(function) * 0.01
+        a += 0.01
+
+    return sum
+
 
 if __name__ == '__main__':
     print(fact(5))
